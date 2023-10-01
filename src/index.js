411cb274d6aa21922b0fa56f85edfede44f36fa4
Gabriela <gabicambuhy@gmail.com>
2023-09-30 22:14:31 -0300

    Adjusting time and timezone

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ function formatDate(timestamp) {
   let now = new Date(timestamp);
   let dayOfWeek = now.getDay();
   let currentHour = now.getHours();
-   if (currentHour < 10) {
-     currentHour = `0${currentHour}`;
-   }
+  if (currentHour < 10) {
+    currentHour = `0${currentHour}`;
+  }
   let currentMinutes = now.getMinutes();
   if (currentMinutes < 10) {
     currentMinutes = `0${currentMinutes}`;
@@ -13,19 +13,19 @@ function formatDate(timestamp) {
   return `${weekDays[dayOfWeek]} ${currentHour}:${currentMinutes}`;
 }
 
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  return days[day];
-}
-
 function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let currentTemp = document.querySelector("#current-temp");
-  let currentWeather = document.querySelector("#current-weather");
-
   currentTemp.innerHTML = `${temperature}°`;
+
+  let timestamp = response.data.dt * 1000;
+  let timezone = response.data.timezone;
+  let formattedDate = formatDate(timestamp, timezone);
+
+  let dateElement = document.querySelector("#current-date");
+  dateElement.innerHTML = formattedDate;
+
+  let currentWeather = document.querySelector("#current-weather");
   currentWeather.innerHTML = response.data.weather[0].main;
 }
 
